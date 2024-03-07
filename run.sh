6632a57e17d6ccf13b063f6367145b60a8aad249
José Luis Landabaso Díaz <landabaso@gmail.com>
2024-03-07 14:42:02 +0100

    Enable CORS for electrs to allow cross-origin requests from Esplora

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ $DIR/run_bitcoind_service.sh
 #Note the 0.0.0.0 -> This is done so that it binds to 0.0.0.0 which means
 #that it can accept calls from any external address. default is 127.0.0.1 which
 #would anly let accept connections within the Docker image
-electrs -vvvv --electrum-rpc-addr 0.0.0.0:60401 --http-addr 0.0.0.0:3002 --daemon-dir /root/.bitcoin --db-dir /root/electrs/db --network regtest &
+electrs -vvvv --electrum-rpc-addr 0.0.0.0:60401 --http-addr 0.0.0.0:3002 --daemon-dir /root/.bitcoin --db-dir /root/electrs/db --network regtest --cors "*" &
 
 export RPCCOOKIE=/root/.bitcoin/regtest/.cookie
 export KEYDB=/root/regtest-data/KEYS
